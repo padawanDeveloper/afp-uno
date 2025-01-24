aa17d7d64e7de659991889218c718e691ac487a5
esteban ortiz <ematiasortiz@gmail.com>
2025-01-24 19:16:26 -0300

    add hook fetch data

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ const BASE_URL = "https://challenge-uno.vercel.app/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
-    headers: { "content-type": "application/x-www-form-urlencoded" },
 });
 
 const get = (url: string) => api.get(url);
