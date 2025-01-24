f2457626ffbed3f5b386961e2b43ad836e2ab0d2
esteban ortiz <ematiasortiz@gmail.com>
2025-01-24 19:18:39 -0300

    add get data home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
+import useFetchData from "@/hooks/useFetchData";
+
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
@@ -11,6 +13,9 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+    const { data, loading } = useFetchData("images");
+
+    console.log({ data, loading });
     return (
         <div
             className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
