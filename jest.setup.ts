3bc771fcaf70683bfeb7580ef542a6460fcd1f17
esteban ortiz <ematiasortiz@gmail.com>
2025-01-27 21:44:05 -0300

    remove jest setup file

--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-// jest.setup.js
-import "@testing-library/jest-dom";
-
-jest.mock("next/image", () => {
-    return ({ src, alt, ...props }) => {
-        // eslint-disable-next-line @next/next/no-img-element
-        return <img src={src} alt={alt} {...props} />;
-    };
-});
